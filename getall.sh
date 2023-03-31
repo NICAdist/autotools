633f31cbae3d4a4757d7120a6226355232feb88d
Jan Busa Jr <jan.busa@gmail.com>
2023-03-31 11:47:21 +0300

    Bumped versions of all packages to latest as of 2023-03-31

--- getall.sh
+++ getall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
-curl -L http://ftp.gnu.org/gnu/m4/m4-1.4.18.tar.gz | tar xzvvf -
-curl -L http://ftpmirror.gnu.org/libtool/libtool-2.4.6.tar.gz | tar xzvvf -
-curl -L http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz | tar xzvvf -
-curl -L http://ftp.gnu.org/pub/gnu/gettext/gettext-0.20.1.tar.gz | tar xzvvf -
-curl -L http://ftp.gnu.org/gnu/automake/automake-1.16.2.tar.gz | tar xzvvf -
+curl -L http://ftp.gnu.org/gnu/m4/m4-1.4.19.tar.gz | tar xzvvf -
+curl -L http://ftpmirror.gnu.org/libtool/libtool-2.4.7.tar.gz | tar xzvvf -
+curl -L http://ftp.gnu.org/gnu/autoconf/autoconf-2.71.tar.gz | tar xzvvf -
+curl -L http://ftp.gnu.org/pub/gnu/gettext/gettext-0.21.1.tar.gz | tar xzvvf -
+curl -L http://ftp.gnu.org/gnu/automake/automake-1.16.5.tar.gz | tar xzvvf -
 curl -L http://pkgconfig.freedesktop.org/releases/pkg-config-0.29.2.tar.gz | tar xzvvf -
-curl -L https://ftp.gnu.org/gnu/help2man/help2man-1.43.2.tar.gz | tar xzvvf -
+curl -L https://ftp.gnu.org/gnu/help2man/help2man-1.49.3.tar.gz | tar xzvvf -
